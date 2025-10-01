0a8e2f82c16f066ae605620d4ac5be1478f66ca6
Mumina Abdi <muminaabdi13@gmail.com>
2025-10-01 16:07:47 -0400

    start to wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,22 @@
+import uuid
+
 class Clothing:
-    pass
+    # WAVE 5
+    def __init__(self, id=None, fabric='Unknown', condition=0):
+        if id is None:
+            # create large unique numbers and make it an interger
+            self.id = uuid.uuid4().int
+        else:
+            # if id already provided, the self.id will just use it
+            self.id = id
+
+        self.fabric = fabric
+        self.condition = condition
+
+    def __str__(self):
+        return (f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric.")
+
+    def get_category(self):
+        return 'Clothing'
+
+# print(Clothing(12345, 'floral'))
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,13 +3,15 @@ import uuid
 # WAVE 2
 class Item:
     # default id to none to start
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id is None:
             # create large unique numbers and make it an interger
             self.id = uuid.uuid4().int
         else:
             # if id already provided, the self.id will just use it
             self.id = id
+
+        self.condition = condition
 
     def get_category(self):
         # we want to see this override in WAVE 5
